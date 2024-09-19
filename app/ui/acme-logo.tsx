13381e2f854ce6fcb7ac16483b70a11e6bda863e
ibrahima <lodiajames@yahoo.com>
2024-09-19 16:52:00 -0400

    fixing fonts error

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,5 +1,9 @@
 import { GlobeAltIcon } from "@heroicons/react/24/outline";
 import { Lusitana } from "next/font/google";
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
 
 export default function AcmeLogo() {
   return (
